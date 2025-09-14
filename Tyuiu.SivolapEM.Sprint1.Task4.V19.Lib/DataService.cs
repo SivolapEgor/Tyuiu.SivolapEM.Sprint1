e6ef9f559e0e157be46f5a801bc1388b67eb8516
Tyuiu.SivolapEM@outlook.com <Tyuiu.SivolapEM@outlook.com>
2025-09-14 22:56:21 +0500

    add Task4

---
+++ Tyuiu.SivolapEM.Sprint1.Task4.V19.Lib/DataService.cs
@@ -0,0 +1,13 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.SivolapEM.Sprint1.Task4.V19.Lib
+{
+    public class DataService : ISprint1Task4V19
+    {
+        public double Calculate(double x, double y)
+        {
+            double res = (x + y) / Math.Abs(x - 2);
+            return Math.Round(res, 3);
+        }
+    }
+}
---
+++ Tyuiu.SivolapEM.Sprint1.Task4.V19/Program.cs
@@ -0,0 +1,39 @@
+﻿using Tyuiu.SivolapEM.Sprint1.Task4.V19.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        double x, y;
+
+        Console.Title = "Спринт #1 | Выполнил: Сиволап Е. М. | ИИПб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Тема: Class Math                                                        *");
+        Console.WriteLine("* Задание #4                                                              *");
+        Console.WriteLine("* Вариант #19                                                             *");
+        Console.WriteLine("* Выполнил: Сиволап Егор Максимович | ИИПб-25-1                           *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+        Console.WriteLine("* вычисляет результат по формуле (x + y) / abs(x - 2) и печатает          *");
+        Console.WriteLine("* его на экране.                                                          *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.Write("\nВведите x -> ");
+        x = Convert.ToDouble(Console.ReadLine());
+        Console.Write("\nВведите y -> ");
+        y = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        double res = ds.Calculate(x,y);
+        Console.WriteLine("Результат (x + y) / abs(x - 2) -> " + res);
+
+    }
+}
